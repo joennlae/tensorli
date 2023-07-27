6e923485ad53e2e9fc7082e940d3aa5a1ef40df0
Jannis Schoenleber <janniss@janniss-m1.local>
2023-07-27 08:41:53 +0200

    style: conform to linter + black

--- tensorli.py
+++ tensorli.py
@@ -1,9 +1,15 @@
-import numpy as np
 from typing import Optional, Union
+import numpy as np
 
-class Tensorli():
-    def __init__(self, data: Union[np.ndarray, float, int], children: Optional[tuple[np.ndarray, np.ndarray]]=(), op: Optional[str] = '') -> None:
-        self.grad : Optional[Tensorli] = None
+
+class Tensorli:
+    def __init__(
+        self,
+        data: Union[np.ndarray, float, int],
+        children: Optional[tuple[np.ndarray, np.ndarray]] = (),
+        op: Optional[str] = "",
+    ) -> None:
+        self.grad: Optional[Tensorli] = None
         if isinstance(data, (float, int)):
             data = np.array([data])
         self.data = data
@@ -15,7 +21,7 @@ class Tensorli():
 
     def __add__(self, other):
         other = other if isinstance(other, Tensorli) else Tensorli(other)
-        out = Tensorli(self.data + other.data, children=(self, other), op = "+")
+        out = Tensorli(self.data + other.data, children=(self, other), op="+")
 
         def _backward():
             self.grad += 1.0 * out.grad
@@ -26,7 +32,7 @@ class Tensorli():
 
     def __mul__(self, other):
         other = other if isinstance(other, Tensorli) else Tensorli(other)
-        out = Tensorli(self.data * other.data, children=(self, other), op = "*")
+        out = Tensorli(self.data * other.data, children=(self, other), op="*")
 
         def _backward():
             self.grad += other.data * out.grad
@@ -37,7 +43,7 @@ class Tensorli():
 
     def __pow__(self, other):
         assert isinstance(other, (float, int)), "only supporting int and float powers for now"
-        out = Tensorli(self.data ** other, children=(self,), op = f"**{other}")
+        out = Tensorli(self.data**other, children=(self,), op=f"**{other}")
 
         def _backward():
             self.grad += (other * self.data ** (other - 1)) * out.grad
@@ -52,7 +58,7 @@ class Tensorli():
         return self + (-other)
 
     def __truediv__(self, other):
-        return self * other ** -1
+        return self * other**-1
 
     def __repr__(self):
         return f"Tensorli(data={self.data}, grad={self.grad})"
