35c4812aa1ea58f229efcce3fa9e811d804917fb
Jannis Schönleber <joennlae@gmail.com>
2023-07-27 10:41:22 +0200

    test: add matmul backprop test compared with pytorch

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,3 +1,5 @@
+import torch
+import numpy as np
 from tensorli import Tensorli
 
 
@@ -8,3 +10,22 @@ def test_simple():
     z.backward()
     assert x.grad.data[0] == 4
     assert y.grad.data[0] == 3
+
+def test_simple_comparision():
+    x_numpy = np.random.randn(10, 10)
+    y_numpy = np.random.randn(10, 10)
+
+    x = Tensorli(x_numpy)
+    y = Tensorli(y_numpy)
+    z = x * y + x
+    z.backward()
+    print(x.grad)
+
+    x_torch = torch.tensor(x_numpy, requires_grad=True)
+    y_torch = torch.tensor(y_numpy, requires_grad=True)
+    z_torch = x_torch * y_torch + x_torch
+
+    z_torch.backward(torch.ones_like(z_torch))
+    print(x_torch.grad)
+
+    assert np.allclose(x.grad, x_torch.grad.numpy())
